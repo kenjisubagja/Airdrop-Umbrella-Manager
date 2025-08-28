424f76eacde3d81296ab66d1e2b98578b1e6be11
kenjisubagja <157840389+kenjisubagja@users.noreply.github.com>
2025-08-28 21:40:35 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 760,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,   
+      sandbox: false,          
+      nodeIntegration: false
+    },
+    title: 'Airdrop Garapan Manager'
+  });
+
+  win.removeMenu();
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
